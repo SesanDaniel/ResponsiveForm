57055533bff77f3c263b1e3e86727e7a045d6acf
unknown <obisesan.seyid@gmail.com>
2022-07-11 12:40:54 +0400

    recent update

---
+++ index.js
@@ -0,0 +1,72 @@
+const form = document.getElementById('form');
+const userFirstName = document.getElementById('firstName');
+const userLastName = document.getElementById('lastName');
+const userEmail = document.getElementById('email');
+const userPassword = document.getElementById('password');
+
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	
+	inputCheck();
+});
+
+function inputCheck() {
+    const userFirstNameValue = userFirstName.value.trim();
+    const userLastNameValue = userLastName.value.trim();
+    const userEmailValue = userEmail.value.trim();
+    const userPasswordValue = userPassword.value.trim();
+
+    if(userFirstNameValue === ''){
+        displayError(userFirstName, 'Firstname cannot be blank');
+    } else {
+        displaySuccess(userFirstName);
+    }
+
+    if(userLastNameValue === ''){
+        displayError(userLastName, 'Last Name cannot be blank')   
+    }else {
+        displaySuccess(userLastName)  
+    }
+
+    if(userEmailValue === ''){
+        displayError(userEmail, 'Email cannot be blank')
+    } else if(!isEmail(userEmailValue)){
+        displayError(userEmail, 'Email is not valid')
+    } else {
+        displaySuccess(userEmail)
+    }
+
+    if(userPasswordValue ===''){
+        displayError(userPassword, 'Password cannot be blank')
+    } else if(userPasswordValue.length < 6){
+        displayError(userPassword, 'Password must be as least 6 characters')
+    } else {
+        displaySuccess(userPassword)
+    }
+}
+    
+    // const btn  = document.getElementById('btn')
+
+//  function lastNote(){
+//     if(inputCheck === displaySuccess)
+//     alert("Thank you! Your free trial is on it's way.... Kindly check your mailbox.")
+//     }
+//   }
+//      lastNote()
+
+function displayError(input, errorMessage) {
+    const formInput = input.parentElement;
+    const small = formInput.querySelector('small');
+    small.innerText = errorMessage;
+    formInput.className = "form-input error";
+}
+
+function displaySuccess(input) {
+    const formInput = input.parentElement;
+    formInput.className = "form-input success";
+} 
+
+function isEmail(userEmail){
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(userEmail);
+ }
